db6eace789cc4f4fa42def2ddbeeb45311cb7864
Mr Canival <31094110+tretap@users.noreply.github.com>
2017-11-02 17:01:33 +0700

    Update Sen
    Update Sentivity

--- Main_compass/Compass(MPU 6050) Python/compass_python.py
+++ Main_compass/Compass(MPU 6050) Python/compass_python.py
@@ -5,6 +5,7 @@ import os
 from compass_imu_Compass_function import *
 
 first = self_test_mag()
+fuse_access_mode_init_()
 
 while(1):
 	os.system('cls' if os.name == 'nt' else 'clear')
@@ -14,9 +15,13 @@ while(1):
 	print("Magnetometer Data.")
 	print("----------")
 
-	hall_xout = read_word_mag_gain(0x04)
-	hall_yout = read_word_mag_gain(0x06)
-	hall_zout = read_word_mag_gain(0x08)
+	#hall_xout = read_word_mag_gain(0x04)
+	#hall_yout = read_word_mag_gain(0x06)
+	#hall_zout = read_word_mag_gain(0x08)
+
+	hall_xout = read_word_mag_sen(0x04,0x10)
+	hall_yout = read_word_mag_sen(0x06,0x11)
+	hall_zout = read_word_mag_sen(0x08,0x12)	
 
 	print(self_test_mag())
 
